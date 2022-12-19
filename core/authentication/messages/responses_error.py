1be3efe64c3af24f6a453aad8f0239e8ac4ba771
gregjose25131 <gregjose2513@gmail.com>
2022-12-19 14:02:35 -0400

    Ordenando proyecto

--- core/authentication/messages/responses_error.py
+++
@@ -1,5 +0,0 @@
-LOGIN_CREDENTIALS_REQUIRED_ERROR = {"message": "Error: Email and Password are required fields"}
-
-LOGIN_CREDENTIALS_ERROR = {"message": "invalid credentials"}
-
-LOGOUT_ERROR = {"Error":"Fields not found"}
--- core/authentication/messages/responses_ok.py
+++
@@ -1,4 +0,0 @@
-LOGIN_OK = "Successful login"
-LOGOUT_OK = "Successful Logout"
-
-SIGNUP_OK = "User created successfully. Now you can login"
