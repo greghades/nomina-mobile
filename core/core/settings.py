96962fe3c78bd39802bb846024cb543b9e1a14e7
gregjose25131 <gregjose2513@gmail.com>
2022-12-19 12:52:19 -0400

    Mergeando solution

--- core/core/settings.py
+++ core/core/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "nomina_mobile",
+        'USER': "Gregori",
+        'PASSWORD': "fender25",
+        'HOST': "localhost",
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
